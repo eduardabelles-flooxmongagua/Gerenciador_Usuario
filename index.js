154ada1feb6f9c5bb366b5ae495a9f7f2641489f
eduardabelles-flooxmongagua <eduarda.belles.flooxmongagua@gmail.com>
2025-02-28 07:45:50 -0300

    feat: add manipulation the DOM

---
+++ index.js
@@ -0,0 +1,15 @@
+
+//Manipulation the DOM
+var nome = document.querySelector("#exampleInputName"); 
+nome.value = "Name"; 
+nome.style.color = "blue"; 
+
+var name = document.querySelector("#exampleInputName");
+var gender = document.querySelectorAll("#form-user-create[name-gender]:checked"); 
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");  
+var email = document.querySelector("#exampleInputEmail"); 
+var passaword = document.querySelector("#exampleInputPassaword"); 
+var photo = document.querySelector("#exampleInputFile"); 
+var admin = document.querySelector("#exampleInputAdmin");
+
